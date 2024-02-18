d7193c33f973f3899c308f496cb1adbcd46534ac
MythiliAadhira <121657043+MythiliAadhira@users.noreply.github.com>
2024-02-18 12:57:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ menu.onclick=() => {
     menu.classList.toggle('fa-items');
     navbar.classList.toggle('active');
 }
+window.onscroll=()=>{
+    menu.classList.remove('fa-items');
+    navbar.classList.remove('active');
+
+
+    if(window.scrollY>60){
+        document.querySelector('#scroll-top').classList.add('active');
+      
+      }
+      else{
+        document.querySelector('#scroll-top').classList.remove('active');
+      
+      }
+}
+function loader(){
+        document.querySelector('.loader-container').classList.add('fade-out');
+}
+      
+function fadeout(){
+        setInterval(loader,3000);
+}
+window.onload=fadeOut();
